648649676ff64f6215febd352d32859cd1104326
infiniityr <valentin.lecompte@gmail.com>
2018-12-10 15:24:48 +0100

    Petites modifs

--- src/mapper.bat
+++ src/mapper.bat
@@ -3,4 +3,5 @@ javac -cp .;../lib/* -sourcepath . Main.java -d ../out/
 
 cd ../out
 
-java -cp .;../lib/* Main "http://localhost" 50000
+java -cp .;../lib/* Main "http://localhost" 50000 -40 -40 40 40
+pause;
