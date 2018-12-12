313e0a5ab119a3a78ff36dc0c388385c3f2094e4
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2018-12-12 16:08:20 +0100

    Minor fixes

--- src/Mapping/Mapper.java
+++ src/Mapping/Mapper.java
@@ -68,9 +68,9 @@ public class Mapper {
 
         for (int x = pos0.getXInt(); x <= pos1.getXInt(); x++) {
             if (x >= 0 && y >= 0 && x < this.map.width && y < this.map.height) {
-                if (this.map.grid[x][y] > 0 && this.map.grid[x][y] <= Map.DEFAULT_GRID_VALUE)
+                if (this.map.grid[x][y] > Map.DEFAULT_GRID_VALUE - 0.01 && this.map.grid[x][y] < Map.DEFAULT_GRID_VALUE + 0.01)
                     this.map.grid[x][y] = 0.0;
-                if (this.map.grid[x][y] > Map.DEFAULT_GRID_VALUE)// && this.map.grid[x][y] < Map.MAX_GRID_VALUE - 0.06)
+                if (this.map.grid[x][y] > Map.DEFAULT_GRID_VALUE && this.map.grid[x][y] < Map.MAX_GRID_VALUE - 0.06)
                     this.map.grid[x][y] -= 0.05;
             } else {
                 break;
@@ -96,9 +96,9 @@ public class Mapper {
 
         for (int y = pos0.getYInt(); y <= pos1.getYInt(); y++) {
             if (x >= 0 && y >= 0 && x < this.map.width && y < this.map.height) {
-                if (this.map.grid[x][y] > 0 && this.map.grid[x][y] <= Map.DEFAULT_GRID_VALUE)
+                if (this.map.grid[x][y] > Map.DEFAULT_GRID_VALUE - 0.01 && this.map.grid[x][y] < Map.DEFAULT_GRID_VALUE + 0.01)
                     this.map.grid[x][y] = 0.0;
-                if (this.map.grid[x][y] > Map.DEFAULT_GRID_VALUE)// && this.map.grid[x][y] < Map.MAX_GRID_VALUE - 0.06)
+                if (this.map.grid[x][y] > Map.DEFAULT_GRID_VALUE && this.map.grid[x][y] < Map.MAX_GRID_VALUE - 0.06)
                     this.map.grid[x][y] -= 0.05;
             } else {
                 return;
