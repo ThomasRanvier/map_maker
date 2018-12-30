e492bb18d694e4670998b472426e784fe8b5d357
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2018-12-30 15:19:07 +0100

    Minor fixes and tests

--- python/mapping/mapper.py
+++ python/mapping/mapper.py
@@ -29,14 +29,14 @@ class Mapper:
                         if laser.echoe < self.__max_distance - self.__safe_distance_obstacle:
                             increment_in_regard_of_distance = self.__increase * (1.0 - (laser.echoe / self.__max_distance))
                             print(inc, increment_in_regard_of_distance)
-                            self.__map.grid[hit_cell.x][hit_cell.y] += inc * increment_in_regard_of_distance * self.__increase
-                            if self.__map.grid[hit_cell.x][hit_cell.y] > 1.0:
-                                self.__map.grid[hit_cell.x][hit_cell.y] = 1.0
+                            self.__map.grid[cell.x][cell.y] += inc * increment_in_regard_of_distance * self.__increase
+                            if self.__map.grid[cell.x][cell.y] > 1.0:
+                                self.__map.grid[cell.x][cell.y] = 1.0
                     else:
                         real_cell = self.__map.to_real_pos(cell)
                         distance = hypot(real_cell.x - real_lasers_cell.x, real_cell.y - real_lasers_cell.y)
                         if distance < self.__max_distance - self.__safe_distance_empty:
                             increment_in_regard_of_distance = self.__increase * (1.0 - (distance / self.__max_distance))
-                            self.__map.grid[hit_cell.x][hit_cell.y] -= inc * increment_in_regard_of_distance * self.__increase
+                            self.__map.grid[cell.x][cell.y] -= inc * increment_in_regard_of_distance * self.__increase
                             if self.__map.grid[cell.x][cell.y] < 0.0:
                                 self.__map.grid[cell.x][cell.y] = 0.0
