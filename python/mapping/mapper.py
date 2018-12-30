512c2852e526104e0deb664b8d0650063c3194b3
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2018-12-30 13:53:59 +0100

    Minor fixes

--- python/mapping/mapper.py
+++ python/mapping/mapper.py
@@ -29,7 +29,9 @@ class Mapper:
             cells = bresenham_line(lasers_cell.x, lasers_cell.y, hit_cell.x, hit_cell.y)
             for cell in cells:
                 if self.__map.is_in_bound(cell):
+                    
                     if cell.x == hit_cell.x and cell.y == hit_cell.y:
+                        """
                         if laser.echoe < self.__max_distance:
                             self.__map.grid[hit_cell.x][hit_cell.y] += self.__increase
                             if self.__map.grid[hit_cell.x][hit_cell.y] > 1.0:
@@ -39,8 +41,9 @@ class Mapper:
                             occupied_probability = self.__occupied_probability(laser.echoe)
                             if not self.__map.grid[hit_cell.x][hit_cell.y] >= 0.7:
                                 occupied_probability += self.__min_increase
-                            self.__map.grid[hit_cell.x][hit_cell.y] = self.__bayesian_probability(occupied_probability, self.__map.grid[hit_cell.x][hit_cell.y])"""
+                            self.__map.grid[hit_cell.x][hit_cell.y] = self.__bayesian_probability(1 - occupied_probability, self.__map.grid[hit_cell.x][hit_cell.y])
                     else:
+                        """
                         self.__map.grid[cell.x][cell.y] -= self.__increase
                         if self.__map.grid[cell.x][cell.y] < 0.0:
                             self.__map.grid[cell.x][cell.y] = 0.0
@@ -49,7 +52,7 @@ class Mapper:
                         distance = hypot(real_cell.x - real_lasers_cell.x, real_cell.y - real_lasers_cell.y)    
                         if distance <= self.__max_distance - 10:
                             occupied_probability = self.__occupied_probability(distance)
-                            self.__map.grid[cell.x][cell.y] = self.__bayesian_probability(1 - occupied_probability, self.__map.grid[cell.x][cell.y])"""
+                            self.__map.grid[cell.x][cell.y] = self.__bayesian_probability(occupied_probability, self.__map.grid[cell.x][cell.y])
 
     def __bayesian_probability(self, occupied_probability, previous_probabilty):
         empty_probability = 1 - occupied_probability
