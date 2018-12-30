51041f2668444b2ec352ed1bb9115470d397dc56
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2018-12-30 14:47:29 +0100

    Minor fixes and tests

--- python/main.py
+++ python/main.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     scale = 1 / size_of_cell_in_meter
 
     robot = Robot(url)
-    lower_left_pos = Position(-50.0, -50.0)
-    upper_right_pos = Position(50.0, 50.0)
+    lower_left_pos = Position(-100.0, -100.0)
+    upper_right_pos = Position(100.0, 100.0)
     robot_map = Map(lower_left_pos, upper_right_pos, scale)
     mapper = Mapper(robot_map)
     show_map = ShowMap(robot_map)
