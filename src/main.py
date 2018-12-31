1b0215c68413351c9fff3a5550cfa542f16dc914
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2018-12-31 20:05:53 +0100

    Abort trying to use multiprocessing for today

--- src/main.py
+++ src/main.py
@@ -9,15 +9,6 @@ from utils.utils import distance_2
 from multiprocessing import Queue, Process
 import time
 
-def run_show_map(queue_show_map, grid, show_map_sleep_time):
-    show_map = ShowMap(grid)
-    while True:
-        robot_map, robot_pos, frontiers, goal_point, path = queue_show_map.get()
-        while not queue_show_map.empty():
-            robot_map, robot_pos, frontiers, goal_point, path = queue_show_map.get()
-        show_map.update(robot_map, robot_map.to_grid_pos(robot_pos), frontiers=frontiers, goal_point=goal_point, path=path)
-        time.sleep(show_map_sleep_time)
-
 def is_goal_reached(goal_point, robot_map, robot_pos, distance_to_trigger_goal_m, size_of_cell_in_meter):
     if goal_point != None:
         dist_2 = distance_2(robot_map.to_grid_pos(robot_pos), goal_point)
@@ -41,26 +32,22 @@ if __name__ == '__main__':
     path_planner = PathPlanner(robot_map)
     cartographer = Cartographer(robot_map)
 
-    queue_show_map = Queue()
-    show_map_process = Process(target=run_show_map, args=(queue_show_map, robot_map.grid, show_map_sleep_time))
-    show_map_process.daemon = True
-    show_map_process.start()
+    show_map = ShowMap(grid)
 
     frontiers = None
     goal_point = None
     path = None
     start = time.time()
-    delay = 2
+    delay = 4
 
     while True:
         robot_pos = robot.position
         robot_lasers = robot.lasers
         cartographer.update(robot_pos, robot_lasers)
-        #show_map.update(robot_map.to_grid_pos(robot_pos), frontiers=frontiers, goal_point=goal_point)
         goal_reached = is_goal_reached(goal_point, robot_map, robot_pos, distance_to_trigger_goal_m, size_of_cell_in_meter)
         if time.time() - start >= delay or goal_reached:
             goal_point, frontiers = goal_planner.get_goal_point(robot_pos)
             path = path_planner.get_path(robot_pos, goal_point)
             start = time.time()
-            delay = 10
-        queue_show_map.put([robot_map, robot_pos, frontiers, goal_point, path])
+            delay = 15
+        show_map.update(robot_map, robot_map.to_grid_pos(robot_pos), frontiers=frontiers, goal_point=goal_point)
