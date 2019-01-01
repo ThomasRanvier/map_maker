6568054d7b73318eeb00e6cd177c31c7d296f934
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2019-01-01 19:58:23 +0100

    Clean up, creation of potential field class

--- src/mapping/cartographer.py
+++ src/mapping/cartographer.py
@@ -20,28 +20,27 @@ class Cartographer:
         real_lasers_cell = self.__map.to_real_pos(lasers_cell)
         count = 0
         for laser in lasers:
-            if count % 2 == 0:
-                continue
             count += 1
-            angle = robot_pos.angle + laser.angle
-            laser_hit = Position(lasers_pos_x + laser.echoe * cos(angle), lasers_pos_y + laser.echoe * sin(angle))
-            hit_cell = self.__map.to_grid_pos(laser_hit)
-            cells = bresenham_line(lasers_cell.x, lasers_cell.y, hit_cell.x, hit_cell.y)
-            for cell in cells:
-                if self.__map.is_in_bound(cell):
-                    if cell.x == hit_cell.x and cell.y == hit_cell.y:
-                        if laser.echoe < self.__max_distance - self.__safe_distance_obstacle:
-                            inc_iro_certainty = self.__min_increment if self.__map.is_empty(cell) else self.__increment
-                            inc_factor_iro_dist = (1.0 - (laser.echoe / self.__max_distance))
-                            self.__map.grid[cell.x][cell.y] += inc_factor_iro_dist * inc_iro_certainty 
-                            if self.__map.grid[cell.x][cell.y] > 1.0:
-                                self.__map.grid[cell.x][cell.y] = 1.0
-                    else:
-                        real_cell = self.__map.to_real_pos(cell)
-                        distance = hypot(real_cell.x - real_lasers_cell.x, real_cell.y - real_lasers_cell.y)
-                        if distance < self.__max_distance - self.__safe_distance_empty:
-                            inc_iro_certainty = self.__min_increment if self.__map.is_obstacle(cell) else self.__increment
-                            inc_factor_iro_dist = (1.0 - (distance / self.__max_distance))
-                            self.__map.grid[cell.x][cell.y] -= inc_factor_iro_dist * inc_iro_certainty
-                            if self.__map.grid[cell.x][cell.y] < 0.0:
-                                self.__map.grid[cell.x][cell.y] = 0.0
+            if count % 2 == 0:
+                angle = robot_pos.angle + laser.angle
+                laser_hit = Position(lasers_pos_x + laser.echoe * cos(angle), lasers_pos_y + laser.echoe * sin(angle))
+                hit_cell = self.__map.to_grid_pos(laser_hit)
+                cells = bresenham_line(lasers_cell.x, lasers_cell.y, hit_cell.x, hit_cell.y)
+                for cell in cells:
+                    if self.__map.is_in_bound(cell):
+                        if cell.x == hit_cell.x and cell.y == hit_cell.y:
+                            if laser.echoe < self.__max_distance - self.__safe_distance_obstacle:
+                                inc_iro_certainty = self.__min_increment if self.__map.is_empty(cell) else self.__increment
+                                inc_factor_iro_dist = (1.0 - (laser.echoe / self.__max_distance))
+                                self.__map.grid[cell.x][cell.y] += inc_factor_iro_dist * inc_iro_certainty 
+                                if self.__map.grid[cell.x][cell.y] > 1.0:
+                                    self.__map.grid[cell.x][cell.y] = 1.0
+                        else:
+                            real_cell = self.__map.to_real_pos(cell)
+                            distance = hypot(real_cell.x - real_lasers_cell.x, real_cell.y - real_lasers_cell.y)
+                            if distance < self.__max_distance - self.__safe_distance_empty:
+                                inc_iro_certainty = self.__min_increment if self.__map.is_obstacle(cell) else self.__increment
+                                inc_factor_iro_dist = (1.0 - (distance / self.__max_distance))
+                                self.__map.grid[cell.x][cell.y] -= inc_factor_iro_dist * inc_iro_certainty
+                                if self.__map.grid[cell.x][cell.y] < 0.0:
+                                    self.__map.grid[cell.x][cell.y] = 0.0
