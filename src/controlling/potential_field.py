b8be0367fc75871d9f8b310fb2441a44ad919397
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2019-01-01 19:48:53 +0100

    Clean up, creation of potential field class

--- src/controlling/potential_field.py
+++ src/controlling/potential_field.py
@@ -1,6 +1,6 @@
 from math import hypot, atan2, cos, sin, inf
 from utils.utils import filled_midpoint_circle
-from logging import logger
+from logging import getLogger
 
 logger = getLogger('potential_field')
 
