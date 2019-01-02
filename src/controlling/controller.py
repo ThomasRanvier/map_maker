7ab62f76233521918acc9b4c143a7a7a703e801b
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2019-01-02 17:13:30 +0100

    Test apply force

--- src/controlling/controller.py
+++ src/controlling/controller.py
@@ -1,4 +1,5 @@
 from logging import getLogger
+from math import hypot, atan2, sin
 
 logger = getLogger('controller')
 
@@ -7,13 +8,22 @@ class Controller:
     Class that implements a Controller, used as an interface to communicate easily with the Robot object which communicates directly with the MRDS server.
     """
 
-    def __init__(self, robot):
+    def __init__(self, robot, max_ang_speed = 3, max_linear_speed = 1, max_ang_speed_weight = 1.5):
         """
         Instantiates a Controller.
         :param robot: The robot to interface.
         :type robot: Robot
+        :param max_ang_speed: The robot max angular speed.
+        :type max_ang_speed: float
+        :param max_linear_speed: The robot max linear speed.
+        :type max_linear_speed: float
+        :param max_ang_speed_weight: The weight to apply to the angular speed.
+        :type max_ang_speed_weight: float
         """
         self.__robot = robot
+        self.__max_ang_speed = max_ang_speed
+        self.__max_linear_speed = max_linear_speed
+        self.__max_speed_weight = max_ang_speed_weight
     
     def apply_force(self, force, robot_pos):
         """
@@ -23,8 +33,12 @@ class Controller:
         :param robot_pos: The position of the robot in the real world.
         :type robot_pos: Position
         """
-        #Convert force vector in angle and length
-        pass
+        force_length = hypot(force['x'], force['y'])
+        force_angle = atan2(force['y'], force['x'])
+        theta = sin(force_angle - robot_pos.angle)
+        ang_speed = self.__max_ang_speed * theta * self.__ang_speed_weight
+        ang_speed = min(max(ang_speed, -self.__max_ang_speed), self.__max_ang_speed)
+        self.__robot.post_speed(ang_speed, 1)
 
     def turn_around(self):
         """
