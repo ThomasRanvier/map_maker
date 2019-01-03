4721664e49a83cd2e74b24b90617a5e37df428f4
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2019-01-03 14:01:15 +0100

    Tests

--- src/controlling/controller.py
+++ src/controlling/controller.py
@@ -46,7 +46,7 @@ class Controller:
             theta = sin(force_angle - robot_pos.angle)
             ang_speed = self.__max_ang_speed * theta * self.__ang_speed_weight
             ang_speed = min(max(ang_speed, -self.__max_ang_speed), self.__max_ang_speed)
-            self.__robot.post_speed(ang_speed, max(0, 0.5 + log10(-x + self.__max_ang_speed)))
+            self.__robot.post_speed(ang_speed, max(0, 0.5 + log10(-ang_speed + self.__max_ang_speed)))
 
     def turn_around(self):
         """
